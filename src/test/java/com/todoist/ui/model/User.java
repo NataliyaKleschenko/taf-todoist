f584f4b522238cfb15b13bac376daacd18d977bb
Nataliya Kleschenko <nataliya.kleschenko@gmail.com>
2023-01-10 09:07:47 +0300

    add allure.properties

--- src/test/java/com/todoist/ui/model/User.java
+++ src/test/java/com/todoist/ui/model/User.java
@@ -3,7 +3,6 @@ package com.todoist.ui.model;
 public class User {
     private final String email;
     private final String password;
-    private String name;
     public User(final String email,final String password) {
         this.email = email;
         this.password = password;
@@ -13,5 +12,31 @@ public class User {
     }
     public String getPassword() {
         return password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (!getEmail().equals(user.getEmail())) return false;
+        return getPassword().equals(user.getPassword());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getEmail().hashCode();
+        result = 31 * result + getPassword().hashCode();
+        return result;
     }
 }
